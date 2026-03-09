59215fa7737f98060efa13cabfbe289e9325710a
BillTheP0ny <kittycity667@gmail.com>
2026-03-09 17:50:56 -0600

    changed ci.yml

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ const app = require('./app.js');
 
 app.listen(config.port, () => {
   console.log(`jwt-pizza-service listening on port ${config.port}`);
+  console.log(`jwt-pizza-service  connecting to db host ${config.db.connection.host}`);
+  console.log(`jwt-pizza-service connecting to pizza factory ${config.factory.url}`);
+
+
 });
